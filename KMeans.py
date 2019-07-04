3a947160a1a640e3f0f680af86cf4fde31da1bc9
Kevin Xu <kevinxu8099@gmail.com>
2019-07-03 23:11:46 -0700

    Clustering Setup

---
+++ KMeans.py
@@ -0,0 +1,6 @@
+t = [[1, 2, 3], [2, 3, 1]]
+print(t)
+
+for i in t:
+    for j in i:
+        print(j)
---
+++ clustering.py
@@ -0,0 +1,10 @@
+class Clustering:
+
+    def __init__(self):
+        self.clusters = []
+        self.mean = []
+
+
+
+
+
