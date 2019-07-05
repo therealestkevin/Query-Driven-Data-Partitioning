7f30460caf9f689f371217ce2a238f407dc1180c
Kevin Xu <kevinxu8099@gmail.com>
2019-07-05 14:53:18 -0700

    Starting MatPlotLib Plotting

--- KMeans.py
+++ KMeans.py
@@ -2,13 +2,14 @@ import random
 import math
 import sys
 import copy
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 from clustering import Clustering
 
 choices = [0, 1]
 
 
 def assigncentroids(clusterings, coorddata, kmeans):
-
     curmeans = []
 
     for i2 in clusterings:
@@ -24,7 +25,7 @@ def assigncentroids(clusterings, coorddata, kmeans):
             dist = euclidean(curcoords, curmeans[j3])
             if dist == minDist:
                 randomchoice = random.choice(choices)
-                if(randomchoice == 1):
+                if (randomchoice == 1):
                     minCluster = j3
 
             elif dist < minDist:
@@ -39,7 +40,6 @@ def reassigncentroids(clusterings, coorddata, kmeans):
     curmeans = []
     tempcopycluster = copy.deepcopy(clusterings)
     changed = 0
-
 
     for i2 in range(len(clusterings)):
         curmeans.append(clusterings[i2].mean)
@@ -68,10 +68,11 @@ def reassigncentroids(clusterings, coorddata, kmeans):
         print(cls.points)
     return changed
 
+
 def recomputemeans(clusterings, coords):
     print("\n New Means:")
     for clster in clusterings:
-        totalcoord = [0]*len(clusterings)
+        totalcoord = [0] * len(clusterings)
         for pointer in clster.points:
             for rownum in range(len(clusterings)):
                 totalcoord[rownum] += coords[rownum][pointer]
@@ -85,9 +86,15 @@ def recomputemeans(clusterings, coords):
 def euclidean(point1, point2):
     totaltemp = 0
     for i3 in range(len(point1)):
-        totaltemp += abs(point1[i3]-point2[i3])**2
+        totaltemp += abs(point1[i3] - point2[i3]) ** 2
     return math.sqrt(totaltemp)
 
+
+colors = ['r', 'g', 'b']
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
 
 threshold = 0.01
 
@@ -112,7 +119,6 @@ for line in f.readlines():
     intValues = []
 
     for i in row:
-
         intValues.append(int(i))
 
     coordData.append(intValues)
@@ -124,7 +130,6 @@ for i in range(k):
     coOrd = []
 
     for j in range(k):
-
         coOrd.append(random.choice(choices))
 
     randoCentroid.append(coOrd)
@@ -134,7 +139,6 @@ print(randoCentroid)
 Clusterings = []
 
 for i in range(k):
-
     temp = Clustering(randoCentroid[i])
 
     Clusterings.append(temp)
@@ -143,18 +147,35 @@ assigncentroids(Clusterings, coordData, k)
 
 print("\nPoints Assigned to Each Cluster: ")
 
-for clusterers in Clusterings:
+for clusterers in range(len(Clusterings)):
+    print(Clusterings[clusterers].points)
+    plottingco = [[]]*3
+    for pnt in Clusterings[clusterers].points:
 
-   print(clusterers.points)
+        for row in range(k):
+            plottingco[row].append(coordData[row][pnt])
+
+    ax.scatter(plottingco[0], plottingco[1], plottingco[2], c=colors[clusterers], marker='o')
+    ax.scatter(Clusterings[clusterers].mean[0], Clusterings[clusterers].mean[1],
+               Clusterings[clusterers].mean[2], c=colors[clusterers], marker='x')
+
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+plt.show()
 
 changed = sys.maxsize
 
-while changed > int((len(coordData[0]) * threshold)):
+iterations = 0
+
+while changed > int((len(coordData[0]) * threshold)) or iterations > maxiterations:
     recomputemeans(Clusterings, coordData)
 
     changed = reassigncentroids(Clusterings, coordData, k)
 
-
+    iterations += 1
 
 #
 # recomputemeans(Clusterings, coordData)
